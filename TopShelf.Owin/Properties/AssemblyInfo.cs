bc50949490b1b4927c4267d6500236e9310c7397
Dennis Roche <dennis.roche@readify.net>
2014-10-23 17:03:15 +0800

    Bump version

--- TopShelf.Owin/Properties/AssemblyInfo.cs
+++ TopShelf.Owin/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
